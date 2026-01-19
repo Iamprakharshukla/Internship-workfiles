73a4794acfdf397861eb3cd67bb75b69c74abbe5
Pooja Patel <poojaspatel1375@gmail.com>
2026-01-19 09:37:59 +0530

    Feat: Implemented Django Groups (Managers vs Staff) to manage permissions. Security: staff1 cannot see the Manager Dashboard. manager1 cannot delete system logs or modify superuser settings.

--- project/settings.py
+++ project/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = "project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
+        "NAME": "C:/Users/Public/udaan.db",
     }
 }
 
